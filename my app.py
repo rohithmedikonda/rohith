5c91a133bdcd48bd31d1355e801b84fcdd9f4c64
rohithmedikonda <147791084+rohithmedikonda@users.noreply.github.com>
2024-01-08 12:17:42 +0530

    Update my app.py

--- my app.py
+++ my app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 st.set_page_config(page_title='Cats')
-st.header("Types of Cats")
+st.header("Types of tiger")
 
 col1, col2 = st.columns(2)
 with col1:
-  st.subheader("r0 cat")
-  st.image("./r0.jpg", caption="Persian Cat", width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.subheader("r0 tiger")
+  st.image("./r0.jpg", caption=" r0 tiger", width=300,use_column_width=True)
+  st.write("r0 tigers are cute")
 with col2:
   st.subheader("R1 Cat")
-  st.image("./r1.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
-  st.write("Ragdoll cats are proud")
+  st.image("./r1.jpg", caption="r1 tiger", width=300,use_column_width=True)
+  st.write("r1 tigers are proud")
